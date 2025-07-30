19e0db0cee5002b907b1d08ba886dcaaee9aa06c
letv1nnn <letv1n2007@icloud.com>
2025-07-30 19:37:53 +0100

    Refactored the repository and added a new program

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-# Computer Vision with C++
+# Free Parking Slot Detection
 
-This repository contains notes and C++ projects related to the OpenCV (Open Source Computer Vision) library. It serves as a personal reference and learning resource for understanding and applying various computer vision techniques using OpenCV.
-
----
-
-## About OpenCV
-
-OpenCV (Open Source Computer Vision Library) is an open-source computer vision and machine learning software library. It provides a wide range of tools for image and video analysis, including features like object detection, face recognition, image filtering, and much more.
-
-Official website: [https://opencv.org/](https://opencv.org/)
+This C++ program uses OpenCV to automatically detect **free parking spots** from a static camera feed or video footage. It highlights occupied and free slots with colored rectangles and displays the count of available spaces on screen.
 
 ---
 
-### Prerequisites
+## How It Works
+Primarily, user need to choose what he is going to use, video or real time camera. After that, user need to calibrate the parking lots, and that basically is the and of configuration.
 
-- C++ Compiler with C++11 (or newer) support
-- [CMake](https://cmake.org/)
-- OpenCV installed (version 4.x recommended)
+### **Stages**:
+...
+
+---
+
+## Requirements
+
+- C++20
+- OpenCV 4.x
+- CMake 3.10+
+
+---
+
+## Building
+...
---
+++ main.cpp
@@ -0,0 +1,130 @@
+// +--------------------------------------------+
+// |    Free Parking Slot Detection program     |
+// | Primarily, it's going to detect free lots  |
+// |   from images, then via camera support.    |
+// +--------------------------------------------+
+
+#include "common.h"
+
+int main(int argc, char **argv) {
+    std::cout << "Running the program...\n";
+
+    std::string path;
+
+    if (argc == 1) {
+        path = "test/parking_lots_video_1.mp4";
+        std::cout << "Using default path: " << path << '\n';
+    } else if (argc == 2) {
+        path = argv[1];
+        std::cout << "Using path: " << path << '\n';
+    } else {
+        std::cout << "Too many arguments!\n";
+    }
+
+    VideoCapture video(path);
+    if (!video.isOpened()) {
+        std::cerr << "Failed to load video!\n";
+        return -1;
+    }
+
+    Mat frame;
+
+    while (true) {
+        video.read(frame);
+
+        const Mat lotsFrame = frame.clone();
+        // you can replace the line above with the line below to look at the parking lots configured by preprocessing function
+        // const Mat lotsFrame = parkingLotLinesDetection(frame);
+        baseImage = lotsFrame.clone();
+        Mat displayFrame = lotsFrame.clone();
+
+        int freeParkingLots = checkLots(displayFrame);
+        std::cout << freeParkingLots << '\n';
+
+        Mat displayWithFooter(displayFrame.rows + EXTRA_HEIGHT, displayFrame.cols, displayFrame.type(), Scalar(0, 0, 0));
+
+        displayFrame.copyTo(displayWithFooter(cv::Rect(0, 0, displayFrame.cols, displayFrame.rows)));
+
+        std::string text = std::format("Free Car Parking Slots: {}", freeParkingLots);
+        putText(displayWithFooter, text, Point(20, displayFrame.rows + 35), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2);
+
+        imshow("Parking Lots Detection Frame", displayWithFooter);
+
+        if (waitKey(0) == 'q') {
+            std::cout << "Quitting...\n";
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+int checkLots(const Mat& frame) {
+    int parkingLotNumber = 1;
+    int freeSpace = 0;
+
+    for (const auto& pt : clickedPoints) {
+
+        Rect roi(pt.x, pt.y, WIDTH, HEIGHT);
+        Mat lot = frame(roi);
+
+        Mat preprocessedLot = preprocessingFrame(lot);
+#if 0
+        imshow("Parking Lots Detection Frame", preprocessedLot);
+        waitKey(0);
+#endif
+        double whiteRatio = calculateWhiteRatio(preprocessedLot);
+
+        std::cout << "Parking space number: " << parkingLotNumber++ << ". White Ratio: " << whiteRatio << '\n';
+
+        std::string ratio = std::format("Ratio: {}", std::round(whiteRatio * 100.0) / 100.0);
+
+        // I need to determine whether the given parking lot is free and then draw a rectangle with appropriate color.
+        if (whiteRatio > 5.5) {
+            rectangle(frame, pt, Point(pt.x + WIDTH, pt.y + HEIGHT), Scalar(0, 0, 255), 2);
+            putText(frame, ratio, Point(pt.x + 5, pt.y + 15), FONT_HERSHEY_SIMPLEX, 0.3, Scalar(0, 0, 255), 1);
+        } else {
+            rectangle(frame, pt, Point(pt.x + WIDTH, pt.y + HEIGHT), Scalar(0, 255, 0), 2);
+            freeSpace++;
+            putText(frame, ratio, Point(pt.x + 5, pt.y + 15), FONT_HERSHEY_SIMPLEX, 0.3, Scalar(0, 255, 0), 1);
+        }
+    }
+
+    return freeSpace;
+}
+
+double calculateWhiteRatio(const Mat& binaryImage) {
+    CV_Assert(binaryImage.type() == CV_8UC1);
+
+    int totalPixels = binaryImage.rows * binaryImage.cols;
+    int whitePixels = countNonZero(binaryImage); // Non-zero = white in binary
+
+    double whiteRatio = static_cast<double>(whitePixels) / totalPixels;
+    return whiteRatio * 100.0; // return as percentage
+}
+
+Mat parkingLotLinesDetection(const Mat& frame) {
+    Mat preprocessedFrame = preprocessingFrame(frame);
+    Mat colorFrame = frame.clone();
+
+    std::vector<Vec4i> lines;
+    HoughLinesP(preprocessedFrame, lines, 1, CV_PI / 180, 50, 30, 5);
+
+    for (size_t i = 0; i < lines.size(); i++) {
+        Vec4i l = lines[i];
+        line(colorFrame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255, 50, 100), 2, LINE_AA);
+    }
+
+    return colorFrame;
+}
+
+Mat preprocessingFrame(const Mat& frame) {
+    Mat preprocessedFrame;
+
+    cvtColor(frame, preprocessedFrame, cv::COLOR_BGR2GRAY);
+    GaussianBlur(preprocessedFrame, preprocessedFrame, Size(3, 3), 0);
+    Canny(preprocessedFrame, preprocessedFrame, 50, 150, 3);
+
+    return preprocessedFrame;
+}
+
